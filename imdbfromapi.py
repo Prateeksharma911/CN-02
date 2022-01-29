ad826470d51dfa3e8b6fdeafc29dbe94a1235178
Prateeksharma911 <prateek1999sharma@gmail.com>
2022-01-29 22:17:46 +0530

    Added exception handling

--- imdbfromapi.py
+++ imdbfromapi.py
@@ -10,23 +10,30 @@ headers = {
     'x-rapidapi-key': "da27635acemsh1b711f00b50d60ap151737jsn1127a9f4ec73"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+try:
+    response = requests.request("GET", url, headers=headers, params=querystring)
+    if response.status_code==200:
+        movielist=(response.json())
+        movielist=movielist['d'][0]['id']
 
-movielist=(response.json())
-movielist=movielist['d'][0]['id']
+        url = "https://www.imdb.com/title/{mname}/".format(mname=movielist)
 
-url = "https://www.imdb.com/title/{mname}/".format(mname=movielist)
+        from bs4 import BeautifulSoup as Soup
 
-from bs4 import BeautifulSoup as Soup
+        movierating = requests.request("GET",url)
+        # Converting url output in text
+        outputimdb=movierating.text
+        print("Getting you data")
+        # parsing html data
+        soup_data=Soup(outputimdb,'html.parser')
+        # Find imdb rating in our html
+        movies = soup_data.findAll('span',{'class':'AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV'})
 
-movierating = requests.request("GET",url)
-# Converting url output in text
-outputimdb=movierating.text
-print("Getting you data")
-# parsing html data
-soup_data=Soup(outputimdb,'html.parser')
-# Find imdb rating in our html
-movies = soup_data.findAll('span',{'class':'AggregateRatingButton__RatingScore-sc-1ll29m0-1 iTLWoV'})
+        imdbrating=str(movies[0])
+        print("Movie rating is :",imdbrating[69:72])
+    else:
+        print("Sorry your movie was not found")
 
-imdbrating=str(movies[0])
-print("Movie rating is :",imdbrating[69:72])
+except requests.exceptions.RequestException as e:
+    print("Something went wrong")
+    raise SystemExit(e)
